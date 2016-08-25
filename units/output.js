7b4b927cdd6e84376d4ece16b6410026e528d266
jmarlin <j.marlin@outlook.com>
2016-08-25 15:40:42 -0400

    testing window nesting

--- units/output.js
+++ units/output.js
@@ -5,7 +5,9 @@ function Output() {
     this.constructor = function(patch) {
 
         var that = new Unit(patch);
+        var child = new WinObj(100, 75, 200, 150);
 
+        that.add_child(child);
         that.resize(200, 150);
 
         var input = that.create_input(5, 75);
