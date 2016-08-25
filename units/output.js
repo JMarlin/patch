3266191b5dfe4a760f036b20258f6bc0af339f61
jmarlin <j.marlin@outlook.com>
2016-08-25 17:43:13 -0400

    Started playing with implementing a slider widget

--- units/output.js
+++ units/output.js
@@ -7,7 +7,7 @@ function Output() {
         var that = new Unit(patch);
         var slider = new Slider(10, 10, 30, 130);
 
-        that.add_child(Slider);
+        that.add_child(slider);
         that.resize(200, 150);
 
         var input = that.create_input(5, 75);
