a9ed1372bdd0f821128fca8c5fd5f80554809d3b
jmarlin <j.marlin@outlook.com>
2016-08-26 17:27:20 -0400

    Slider value getter correct operation confirmed, removed matching output.js test code

--- units/output.js
+++ units/output.js
@@ -7,18 +7,6 @@ function Output() {
         var that = new Unit(patch);
         var slider = new Slider(10, 10, 30, 130, 0, 1);
 
-        that.old_mouseup = that.onmouseup;
-        that.onmouseup = function() {
-        
-            slider.value = 0.75;
-            that.old_mouseup();
-        };
-
-        setInterval(function() {
-
-            console.log(slider.value);
-        }, 1);
-
         that.add_child(slider);
         that.resize(200, 150);
 
