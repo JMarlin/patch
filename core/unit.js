db6ac321ae9017824e0edfc4b4d403b4df0b4e60
jmarlin <j.marlin@outlook.com>
2016-08-29 16:55:47 -0400

    Added output spawning to Unit class

--- core/unit.js
+++ core/unit.js
@@ -13,6 +13,16 @@ function Unit(patch) {
         if(that.invalidate) that.invalidate();
     };
 
+    that.create_output = function(x, y) {
+
+        var output = new Output(patch, x, y);
+
+        that.outputs.push(output);
+        that.add_child(output);
+
+        return output;
+    }
+
     that.create_input = function(x, y) {
 
         var input = new Input(patch, x, y);
