6a678472fd4c119a5a1ae8c857ba49f844852e7a
Joseph Marlin <j.marlin@outlook.com>
2017-01-19 22:56:52 +0000

    Finished first pass at writing basic serialify primitive functions

--- cver/serialify/serialify.c
+++ cver/serialify/serialify.c
@@ -67,6 +67,26 @@ int Serialify_insert_bytes(SerialifyBuf* sbuf, int count, uint8_t* inbuf) {
     return i;
 }
 
+int Serialify_peek_bytes(SerialifyBuf* sbuf, int count, uint8_t* outbuf) {
+
+    int i;
+
+    for(i = 0; (i < count) && ((i + sbuf->loc) < sbuf->used_size); i++)
+        outbuf[i] = sbuf->buffer_base[sbuf->loc + i];
+
+    return i;
+}
+
+int Serialify_read_bytes(SerialifyBuf* sbuf, int count, uint8_t* outbuf) {
+
+    int i;
+
+    i = Serialify_peek_bytes(sbuf, count, outbuf);
+    sbuf->loc += i;
+
+    return i;
+}
+
 int Serialify_from_cstring(SerialifyBuf* sbuf, char* string) {
 
     return Serialify_insert_bytes(sbuf, strlen(string) + 1, string);
@@ -162,12 +182,147 @@ int Serialify_from_double(SerialifyBuf* sbuf, double value) {
     return Serialify_insert_bytes(sbuf, 8, vbuf);
 }
 
-char* Serialify_to_cstring(SerialifyBuf* sbuf);
-int8_t Serialify_to_int8(SerialifyBuf* sbuf);
-uint8_t Serialify_to_uint8(SerialifyBuf* sbuf);
-int16_t Serialify_to_int16(SerialifyBuf* sbuf);
-uint16_t Serialify_to_uint16(SerialifyBuf* sbuf);
+char* Serialify_to_cstring(SerialifyBuf* sbuf) {
+
+    int i;
+    int strlen = 0;
+    char* outstr;
+    uint8_t cbuf = 1;
+
+    while(cbuf) {
+
+        Serialify_peek_bytes(sbuf, 1, &cbuf);
+        strlen++;
+    }
+
+    outstr = (char*)malloc(strlen);
+
+    if(!outstr)
+        return outstr;
+
+    Serialify_read_bytes(sbuf, strlen, (uint8_t)outstr);
+
+    return outstr;
+}
+
+int8_t Serialify_to_int8(SerialifyBuf* sbuf) {
+
+    int8_t outval;
+
+    Serialify_read_bytes(sbuf, 1, (uint8_t*)&outval);
+
+    return outval;
+}
+
+uint8_t Serialify_to_uint8(SerialifyBuf* sbuf) {
+
+    uint8_t outval;
+    
+    Serialify_read_bytes(sbuf, 1, &outval);
+
+    return outval;
+}
+
+int16_t Serialify_to_int16(SerialifyBuf* sbuf) {
+
+    int16_t outval;
+    uint16_t* uval;
+    uint8_t vbuf[2];
+
+    Serialify_read_bytes(sbuf, 2, &vbuf);
+
+    uval = (uint16_t*)&outval;
+    *uval = 0;
+    *uval |= vbuf[0] << 8;
+    *uval |= vbuf[1];
+
+    return outval;
+}
+
+uint16_t Serialify_to_uint16(SerialifyBuf* sbuf) {
+
+    uint16_t outval;
+    uint8_t vbuf[2];
+
+    Serialify_read_bytes(sbuf, 2, &vbuf);
+
+    outval = 0;
+    outval |= vbuf[0] << 8;
+    outval |= vbuf[1];
+
+    return outval;
+}
+
 int32_t Serialify_to_int32(SerialifyBuf* sbuf);
+
+    int32_t outval;
+    uint32_t* uval;
+    uint8_t vbuf[4];
+
+    Serialify_read_bytes(sbuf, 4, &vbuf);
+
+    uval = (uint32_t*)&outval;
+    *uval = 0;
+    *uval |= vbuf[0] << 24;
+    *uval |= vbuf[1] << 16;
+    *uval |= vbuf[2] << 8;
+    *uval |= vbuf[3];
+
+    return outval;
+}
+
 uint32_t Serialify_to_uint32(SerialifyBuf* sbuf);
+
+    uint32_t outval;
+    uint8_t vbuf[4];
+
+    Serialify_read_bytes(sbuf, 4, &vbuf);
+
+    outval = 0;
+    outval |= vbuf[0] << 24;
+    outval |= vbuf[1] << 16;
+    outval |= vbuf[2] << 8;
+    outval |= vbuf[3];
+
+    return outval;
+}
+
 float Serialify_to_float(SerialifyBuf* sbuf);
+
+    float outval;
+    uint32_t* uval;
+    uint8_t vbuf[4];
+
+    Serialify_read_bytes(sbuf, 4, &vbuf);
+
+    uval = (uint32_t*)&outval;
+    *uval = 0;
+    *uval |= vbuf[0] << 24;
+    *uval |= vbuf[1] << 16;
+    *uval |= vbuf[2] << 8;
+    *uval |= vbuf[3];
+
+    return outval;
+}
+
 double Serialify_to_double(SerialifyBuf* sbuf);
+
+    double outval;
+    uint64_t* uval;
+    uint8_t vbuf[8];
+
+    Serialify_read_bytes(sbuf, 8, &vbuf);
+
+    uval = (uint64_t*)&outval;
+    *uval = 0;
+    *uval |= vbuf[0] << 56;
+    *uval |= vbuf[1] << 48;
+    *uval |= vbuf[2] << 40;
+    *uval |= vbuf[3] << 32;
+    *uval |= vbuf[4] << 24;
+    *uval |= vbuf[5] << 16;
+    *uval |= vbuf[6] << 8;
+    *uval |= vbuf[7];
+
+    return outval;
+}
